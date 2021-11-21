dab36942f6b5727ada0351e17d50858953647b7e
Christopher Jagielski <8332583+ChristopherJagielski@users.noreply.github.com>
2021-11-21 21:05:48 +0100

    Update config.js
    populated config file.

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "603d07fhamntmhnfifmaj61mcr",     // CognitoClientID
+  "api_base_url": "https://5h7yt7evl1.execute-api.eu-west-1.amazonaws.com/prod",     // TodoFunctionApi
+  "cognito_hosted_domain": "mylambdaapptest-mylambdaapptest1.auth.eu-west-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1kcsytontmcwp.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
